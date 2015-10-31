c55f851bedfde352d625e831bc9c13786a31fa58
Jake White <squirrelboy1225@gmail.com>
2015-10-30 22:41:23 -0400

    Got pygame working
    Installed pygame, added instructions to readme, etc.

---
+++ gamelogic.py
@@ -0,0 +1,39 @@
+import time
+
+
+class GameLoop(object):
+    shouldBeRunning = True
+    interval = 10
+
+    def __init__(self, interval, screen):
+        # requires a looping interval (ms) and a screen object
+        self.interval = interval
+        self.screen = screen
+
+    def start(self):
+        print("Loop starting...")
+        self.timeSinceTick = self.interval
+        currentTime = time.time()
+        while self.shouldBeRunning:
+            self.timeSinceTick = timeInMillis() - currentTime
+            #print(timeElapsed)
+            if (self.timeSinceTick >= self.interval):
+                self.tick()
+                currentTime = timeInMillis()
+            #print(currentTime)
+
+    def stop(self):
+        self.shouldBeRunning = False
+
+    def tick(self):
+        self.screen.draw()
+
+    def getInterval(self):
+        return self.Sinterval
+
+    def getTimeSinceTick(self):
+        return self.timeSinceTick
+
+
+def timeInMillis():
+    return time.time() * 1000
--- render.py
+++ render.py
@@ -1,7 +1,21 @@
-import graphics
+import pygame, sys
 
-class Screen(object):
-    def __init__(self):
-        window = graphics.GraphWin()
+class Screen():
+    def __init__(self, title, width, height):
+        pygame.init()
+        self.screen = pygame.display.set_mode((640,480))
+        self.white = (255,255,255)
 
     def draw(self):
+        print("drawn!")
+        self.screen.fill(self.white)
+        pygame.display.update()
+        self.eventCatcher()
+
+    def eventCatcher(self):
+        #catching pygame generated events
+        for event in pygame.event.get():
+            #closes both the pygame module and forces the program to end
+            if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
